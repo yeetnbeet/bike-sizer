4ed27d135c7ed2c094d38cdee5b1bea6a5959128
mostlyclicks <carlos@mostlyclicks.com>
2021-11-20 21:36:33 -0700

    style intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Intro = (props) => {
   return (
-    <div className={props.classes}>
-      <h1>Tell us some basic measurements</h1>
-      <p>To find the right size bike for you, we'll need some basic size info:</p>
-      <ul>
+    <div className={`${props.classes} `}>
+      <h1 className="text-2xl font-medium mb-4 leading-6">Your Measurements</h1>
+      <p className="text-sm">To find the right size bike for you, we'll need some basic size info:</p>
+      <ul className="list-disc list-inside text-sm my-4">
         <li>Your height</li>
         <li>Your inseam</li>
         <li>Your upper body height</li>
       </ul>
-      <p>It will also be helpful to get the saddle height and saddle to bar measurement from your current bike (if possible).</p>
+      <p class="text-sm">It will also be helpful to get the saddle height and saddle to bar measurement from your current bike (if possible).</p>
     </div>
   )
 }
