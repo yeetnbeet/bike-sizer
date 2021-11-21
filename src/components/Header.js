7c3c323e718a3474ca727bee89eaa51f6fcadec0
mostlyclicks <carlos@mostlyclicks.com>
2021-11-20 21:13:33 -0700

    add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <header className={`${props.classes}`}>
-      <h1>Contender Bike Sizer</h1>
+    <header className={`${props.classes} h-16 p-4 bg-red-600 rounded-lg my-4 text-white shadow-lg`}>
+      <h1 className="text-2xl font-bold">Contender Bike Sizer</h1>
 
       
     </header>
