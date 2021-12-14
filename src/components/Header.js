2ffabe9ca0b295aae340455c389c1b2e8d51f8e2
mostlyclicks <carlos@mostlyclicks.com>
2021-12-14 10:15:43 -0700

    add logo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../images/contender-logo-white.png'
 
 const Header = (props) => {
   return (
-    <header className={`${props.classes} h-16 p-4 bg-red-600 rounded-lg my-4 text-white shadow-lg`}>
-      <h1 className="text-2xl font-bold">Contender Bike Sizer</h1>
+    <header className={`${props.classes} h-24 p-4 px-8 bg-red-600 rounded-md my-4 text-white shadow-lg flex justify-between items-center`}>
+      <img src={logo} alt="Contender Bicycles Logo" className="h-full" /> 
+      <h1 className="text-md sm:text-2xl font-bold">Contender Bike Sizer</h1>
 
       
     </header>
