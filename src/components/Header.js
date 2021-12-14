b5821dcbb4ce55d806e3f246c60bc709644d8357
mostlyclicks <carlos@mostlyclicks.com>
2021-12-14 10:53:04 -0700

    fix header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import logo from '../images/contender-logo-white.png'
 const Header = (props) => {
   return (
     <header className={`${props.classes} h-36 sm:h-24 p-4 px-4 sm:px-8 bg-red-600 rounded-md my-4 text-white shadow-lg flex sm:flex-row flex-col justify-between items-center`}>
-      <img src={logo} alt="Contender Bicycles Logo" className="h-full" /> 
-      <h1 className="mt-4 sm:mt-0 text-md sm:text-2xl font-bold">Contender Bike Sizer</h1>
+      <img src={logo} alt="Contender Bicycles Logo" className="h-1/2 sm:h-full" /> 
+      <h1 className="mt-4 sm:mt-0 text-xl sm:text-2xl font-bold">Contender Bike Sizer</h1>
 
       
     </header>
