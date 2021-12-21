f47e251e469f4034a8c13a1304e2cca38851f425
mostlyclicks <carlos@mostlyclicks.com>
2021-12-21 10:44:03 -0700

    add dividing border to output

--- src/components/Output.js
+++ src/components/Output.js
@@ -6,22 +6,22 @@ const Output = (props) => {
 
   if(user.height>=0 && user.inseam>=0 && user.torsoLength>=0){
   return (
-    <div className={`${props.classes} bg-gray-100 rounded-lg h-96 flex flex-col justify-around lg:flex-row sm:text-3xl items-center`}>
+    <div className={`${props.classes} bg-gray-100 rounded-lg h-auto flex flex-col justify-around lg:flex-row sm:text-3xl items-center`}>
 
-      <div className="">
-        <h1>Your Measurements</h1>
+      <div className="py-8">
+        <h1 className="text-xl">Your Measurements</h1>
         <p className="text-lg"><strong>Name:</strong> {user.name}</p>
         <p className="text-lg"><strong>Height:</strong> {user.height}</p>
         <p className="text-lg"><strong>Inseam:</strong> {user.inseam}</p>
         <p className="text-lg"> <strong>Torso Length:</strong> {user.torsoLength}</p>
       </div>
 
-      <div className="text-center flex flex-col justify-end items-stretch">
-        <h1 className="">Your Suggested Bike Measurements</h1>
+      <div className="py-8 border-t-2 border-gray-300 lg:border-l-2 lg:border-t-0 lg:pl-16 text-center flex flex-col justify-end items-stretch">
+        <h1 className="text-xl">Your Suggested Bike Measurements</h1>
         <div className="flex justify-evenly space-x-2">
-        <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl p-8 block">{Math.round(user.riderFit.saddleHeight*10)/10}</span><strong className="block">Saddle Height</strong> </p>
-        <p className="text-lg block text-center"><span className="text-2xl md:text-4xl p-8 block ">{user.riderFit.stackHeight[0]} to {user.riderFit.stackHeight[1]}</span><strong className="block">Stack Height</strong> </p>
-        <p className="text-lg block text-center"><span className="text-2xl md:text-4xl p-8 block ">{Math.round(user.riderFit.reach*10)/10}</span><strong className="block">Reach</strong></p>
+        <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block">{Math.round(user.riderFit.saddleHeight*10)/10}</span><strong className="block">Saddle Height</strong> </p>
+        <p className="text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block ">{user.riderFit.stackHeight[0]} to {user.riderFit.stackHeight[1]}</span><strong className="block">Stack Height</strong> </p>
+        <p className="text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block ">{Math.round(user.riderFit.reach*10)/10}</span><strong className="block">Reach</strong></p>
         </div>
       </div>
       
