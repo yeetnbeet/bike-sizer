f65dbf6c52e06834e03517232a1759594dae02c2
mostlyclicks <carlos@mostlyclicks.com>
2021-12-22 11:15:25 -0700

    add empty key.json file

--- src/components/Output.js
+++ src/components/Output.js
@@ -21,7 +21,10 @@ const Output = (props) => {
         <div className="flex justify-evenly space-x-2">
         <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block">{Math.round(user.riderFit.saddleHeight*10)/10}</span><strong className="block">Saddle Height</strong> </p>
         <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block ">{user.riderFit.stackHeight[0]} to {user.riderFit.stackHeight[1]}</span><strong className="block">Stack Height</strong> </p>
-        <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block ">{Math.round(user.riderFit.reach*10)/10}</span><strong className="block">Reach</strong></p>
+        <p className="text-base sm:text-lg block text-center"><span className="text-2xl md:text-4xl px-8 py-6 block ">{Math.round(user.riderFit.reach[0]*10)/10} to {Math.round(user.riderFit.reach[1]*10)/10}</span><strong className="block">Reach</strong></p>
+       
+
+
         </div>
       </div>
       
