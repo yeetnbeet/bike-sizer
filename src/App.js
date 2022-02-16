efbe153fec97d7c7d66192b9d93ba898284667d4
Ryan Littlefield <segalcsam@gmail.com>
2022-02-16 16:00:16 -0700

    -header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import Header from './components/Header'
+
 import Intro from './components/Intro'
 import SizeForm from './components/SizeForm';
 import Output from './components/Output'
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div className="container mx-auto px-8 flex flex-col sm:flex-row flex-wrap">
-      <Header classes="w-full"/>
+      
       <Intro classes="sm:w-1/2 p-4"/>
       <SizeForm onFormSubmit={displaySize} classes="sm:w-1/2 p-4" />
       <Convert classes="sm:w-1/2 p-4"/>
